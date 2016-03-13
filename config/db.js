5bde0e555cb8d933f1157963ab949dfd04b8a188
micheleclarkson <micheleclarkson@rogers.com>
2016-03-13 16:47:47 -0400

    CRUD Complete
    Linking to Heroku

---
+++ config/db.js
@@ -0,0 +1,9 @@
+//create a public connection string
+module.exports = {
+    
+    //local
+    //'url': 'mongodb://localhost/test'
+    
+    //live on mLab
+    'url': 'mongodb://micheleClarkson:moogie@ds064278.mlab.com:64278/michele2106'
+};
